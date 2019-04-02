c7b5e0dd380ec4e98eb973077e164e7803a5657b
IvanVynyavsky <ivanvynyavsky@gmail.com>
2019-04-02 12:18:19 +0300

    Create print function

--- Menu/Menu/Menu.cs
+++ Menu/Menu/Menu.cs
@@ -8,6 +8,25 @@ namespace Menu
 {
     public class Menu
     {
+        void print()
+        {
+            Console.Clear();
+            Console.WriteLine("Enter 1 to add employee ");
+            Console.WriteLine("Enter 2 to add department ");
+            Console.WriteLine("Enter 3 to delete exactly employee ");
+            Console.WriteLine("Enter 4 to delete exactly department ");
+            Console.WriteLine("Enter 5 to delete all employees ");
+            Console.WriteLine("Enter 6 to delete all departments ");
+            //
+
+            //
+            Console.WriteLine("Enter 7 to edit employee ");
+            Console.WriteLine("Enter 8 to edit department ");
+            Console.WriteLine("Enter 9 to print all employee ");
+            Console.WriteLine("Enter 10 to print all department ");
+            Console.WriteLine("Enter 11 to exit ");
+
+        }
 
     }
 }
