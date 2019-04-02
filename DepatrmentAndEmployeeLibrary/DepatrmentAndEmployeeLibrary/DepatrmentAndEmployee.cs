720629b7200915d9f6e6966fe36fdc89c8ce1606
Roman Kunovskyi <romankunovskyy@gmail.com>
2019-04-02 12:15:35 +0300

    Info and Lists

---
+++ DepatrmentAndEmployeeLibrary/DepatrmentAndEmployeeLibrary/DepatrmentAndEmployee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepatrmentAndEmployeeLibrary
+{
+    public class DepatrmentAndEmployee
+    {
+        public DepatrmentAndEmployee()
+        {
+            EmployeeInfoList = new List<EmployeeInfo>();
+            DepartmentInfoList = new List<DepartmentInfo>();
+        }
+
+        public class EmployeeInfo
+        {
+            public long Id { get; set; }
+            public long DepartmentId { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public long PhoneNumber { get; set; }
+            public DateTime HasBeenWorkSince { get; set; }
+        }
+        public List<EmployeeInfo> EmployeeInfoList;
+        public class DepartmentInfo
+        {
+            public long Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
+            public long PhoneNumber { get; set; }
+            public DateTime DateOfCreationDepartment { get; set; }
+        }
+        public List<DepartmentInfo> DepartmentInfoList;
+    }
+}
